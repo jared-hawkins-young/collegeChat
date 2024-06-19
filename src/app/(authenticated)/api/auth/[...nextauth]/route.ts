dbd762a83691ab93b01d7a8fb41078d31e950bfb
jared-hawkins-young <143453407+jared-hawkins-young@users.noreply.github.com>
2024-06-18 20:08:10 -0400

    route.ts

--- src/app/(authenticated)/api/auth/[...nextauth]/route.ts
+++ src/app/(authenticated)/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,3 @@
-/*import { handlers } from "@/features/auth-page/auth-api";
+import { handlers } from "@/features/auth-page/auth-api";
 
-export { handlers as GET, handlers as POST };*/
+export { handlers as GET, handlers as POST };
