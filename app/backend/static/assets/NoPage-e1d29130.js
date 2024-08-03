370e7b0f45f1262647bdd7a224d378bbb9aa2a8a
jared-hawkins-young <143453407+jared-hawkins-young@users.noreply.github.com>
2024-08-03 13:20:18 -0400

    feat: over writing with new code

---
+++ app/backend/static/assets/NoPage-e1d29130.js
@@ -0,0 +1,2 @@
+import{bC as o}from"./vendor-9f0443cf.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
+//# sourceMappingURL=NoPage-e1d29130.js.map
